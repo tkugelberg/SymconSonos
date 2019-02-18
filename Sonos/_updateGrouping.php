4c6ba41a2ec6772c7c3ac66fb9980722d9d6ae75
Brovning <39644775+Brovning@users.noreply.github.com>
2019-02-18 20:01:20 +0100

    Undefined variable: group
    If you have grouped Sonos players, there will be an error in line 47, because $group is not defined.

--- Sonos/_updateGrouping.php
+++ Sonos/_updateGrouping.php
@@ -25,6 +25,7 @@ IPS_SetScriptTimer($_IPS["SELF"], $frequency);
 $sonos = new SonosAccess($ipAddress);
 
 $grouping = new SimpleXMLElement($sonos->GetZoneGroupState());
+$group = 0;
 
 foreach($allSonosInstances as $key=>$SonosID) {
     $rincon = IPS_GetProperty($SonosID ,"RINCON");
